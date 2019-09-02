9d6e6b76f069449637dac7ae30e570643a3d604c
이규종 <kyujong93@naver.com>
2019-09-02 16:20:15 +0900

    add cors

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import routes from './routes';
 import userRouter from './routes/userRouter';
+import cors from 'cors';
 
 const app = express();
 
 app.use(helmet());
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('dev'));
