3ded777d24951f60d488628adb28c0fbe66fd06a
이규종 <kyujong93@naver.com>
2019-10-20 12:47:00 +0900

    authRouter getToken 설정 작성

--- src/router/authRouter.js
+++ src/router/authRouter.js
@@ -3,6 +3,38 @@ import routes from '../routes';
 import { postLogin } from '../controllers/authController';
 
 const authRouter = express.Router();
+/**
+ * @swagger
+ * tags:
+ *     name: JwtToken
+ *     description: login 시 jwt token 얻기
+ */
+
+/**
+ * @swagger
+ * /auth/getToken:
+ *   post:
+ *     summary: Returns token
+ *     tags : [JwtToken]
+ *     parameters: 
+ *         - in: query
+ *           name: email
+ *           schema: 
+ *               type: string
+ *           description: 이메일
+ *         - in: query
+ *           name: password
+ *           schema: 
+ *              type: string
+ *     responses:
+ *       200:
+ *         description: jwt token
+ *         schema:
+ *           type: string
+ *           properties:
+ *             token:
+ *               type: string
+ */
 
 authRouter.post(routes.getToken, postLogin);
 
