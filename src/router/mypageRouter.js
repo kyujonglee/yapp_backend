a7f6a63f655cdef08aeb613d850495b28b2de303
이규종 <kyujong93@naver.com>
2019-10-29 23:09:19 +0900

    [Add] parseAndDeleteArray & mypage delete keyword api complete
    parseAndDeleteAraay :
    1,2,3과 같이 문자열로 되어있는 것을 배열로 바꾼 뒤 해당 id를 삭제함.

--- src/router/mypageRouter.js
+++ src/router/mypageRouter.js
@@ -2,25 +2,53 @@ import express from 'express';
 import routes from '../routes';
 import { getSupports } from '../controllers/mypageController';
 import { onlyPrivate } from '../middlewares';
-import { getKeywords, chooseKeyword } from '../controllers/keywordController';
+import {
+  getKeywords,
+  chooseKeyword,
+  deleteKeyword
+} from '../controllers/keywordController';
 
 const mypageRouter = express.Router();
 
 mypageRouter.get(routes.supports, onlyPrivate, getSupports);
 mypageRouter.get(routes.keywords, getKeywords);
-mypageRouter.put(
+mypageRouter.post(
   `${routes.keywords}${routes.keywordId}`,
   onlyPrivate,
   chooseKeyword
+);
+mypageRouter.delete(
+  `${routes.keywords}${routes.keywordId}`,
+  onlyPrivate,
+  deleteKeyword
 );
 
 /**
  * @swagger
  * /mypage/keywords/{keywordId}:
- *   put:
+ *   delete:
+ *     summary: 사용자가 keyword를 삭제
+ *     tags: [Keyword]
+ *     parameters:
+ *         - in : path
+ *           name: keywordId
+ *           schema:
+ *              type: integer
+ *           required: true
+ *     responses:
+ *       200:
+ *         description: Keyword list
+ *         schema:
+ *           type: boolean
+ */
+
+/**
+ * @swagger
+ * /mypage/keywords/{keywordId}:
+ *   post:
  *     summary: 사용자가 keyword를 등록
  *     tags: [Keyword]
- *     parameters: 
+ *     parameters:
  *         - in : path
  *           name: keywordId
  *           schema:
